586050dc92b61470fe54eb827bb145d10bf7e47d
BPitts8019 <bradleyj.pitts@gmail.com>
2019-08-07 18:22:28 -0700

    Added start and reset buttons to timer

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,50 +1,76 @@
 (() => {
    const MAX_TIME = 10000;
+   const btnStart = document.querySelector("#start");
+   const btnReset = document.querySelector("#reset");
    const digits = [
       document.querySelector("#secondTens"),
       document.querySelector("#secondOnes"),
       document.querySelector("#msHundreds"),
       document.querySelector("#msTens")
    ];
+   let timer;
    let numMiliseconds = 0;
+
+   const resetTimer = () => {
+      if (timer) clearInterval(timer);
+      numMiliseconds = 0;
+
+      digits.forEach(digit => {
+         digit.textContent = "0";
+         digit.classList.remove("redDigit");
+      });
+      document.querySelector("#colon").classList.remove("redDigit");
+
+      btnStart.disabled = false;
+   };
+
+   const runTimer = () => {
+      //UI handler
+      const updateUI = num => {
+         const MAX_DIGITS = 4;
+         let remainingDigits = num;
    
-   //UI handler
-   const updateUI = num => {
-      const MAX_DIGITS = 4;
-      let remainingDigits = num;
-
-      for (let i = 0; i < MAX_DIGITS; i++) {
-         const comparator = Math.pow(10, MAX_DIGITS - (i+1));
-         const digit = Math.floor(remainingDigits / comparator);
-         digits[i].textContent = digit;
-
-         if (digit > 0) {
-            remainingDigits %= comparator;
+         for (let i = 0; i < MAX_DIGITS; i++) {
+            const comparator = Math.pow(10, MAX_DIGITS - (i+1));
+            const digit = Math.floor(remainingDigits / comparator);
+            digits[i].textContent = digit;
+   
+            if (digit > 0) {
+               remainingDigits %= comparator;
+            }
          }
-      }
+      };
+      //interval handler
+      const updateTimer = () => {
+         //update counter
+         numMiliseconds += 10;
+   
+         //update UI
+         updateUI(numMiliseconds / 10); //only work with the first 4 digits
+   
+         //stop timer if necessary
+         if (numMiliseconds >= MAX_TIME) {
+            clearInterval(timer);
 
-      // console.log(num);
+            digits.forEach(digit => {
+               digit.classList.add("redDigit");
+            });
+            document.querySelector("#colon").classList.add("redDigit");
+
+            btnStart.disabled = false;
+         }
+      };
+
+      //reset timer
+      resetTimer();
+      btnStart.disabled = true;
+
+      //start interval
+      timer = setInterval(updateTimer, 10);
+      // window.updateUI = updateUI;
    };
 
-   //interval handler
-   const updateTimer = () => {
-      //update counter
-      numMiliseconds += 10;
-
-      //update UI
-      updateUI(numMiliseconds / 10); //only work with the first 4 digits
-
-      //stop timer if necessary
-      if (numMiliseconds >= MAX_TIME) {
-         clearInterval(timer);
-         digits.forEach(digit => {
-            digit.style.color = "red";
-         });
-         document.querySelector("#colon").style.color = "red";
-      }
-   };
-
-   //start interval
-   let timer = setInterval(updateTimer, 10);
-   // window.updateUI = updateUI;
+   //setup event listeners
+   btnStart.addEventListener("click", runTimer);
+   btnReset.addEventListener("click", resetTimer);
 })();
